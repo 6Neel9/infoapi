63880f0ea5a63bad8e997753ad20086b66a5066e
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-08-09 13:00:26 +0530

    Update info.js

--- Routes/info.js
+++ Routes/info.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Data = require('../Modules/model');
+const {Data} = require('../Modules/model');
 
 //get api
 router.get('/', async (req, res) => {
