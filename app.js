1ca1d66bbf40703f87ebee6c6611e8d59c11c49d
6Neel9 <88641136+6Neel9@users.noreply.github.com>
2022-08-10 19:38:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ var PORT = process.env.PORT || 4000;
 
 
 //Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
